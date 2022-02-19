1a63bc214aac30bacc265b2489ae12459316ac15
Guilherme <Guilherme@DESKTOP-VSM31N4>
2022-02-19 19:23:53 -0300

    Impletado o summary dos produtos

--- PointStore.Domain/Entity/Produto.cs
+++ PointStore.Domain/Entity/Produto.cs
@@ -8,10 +8,25 @@ namespace PointStore.Domain.Entity
 {
     public class Produto
     {
+        /// <summary>
+        /// Id do Produto
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Nome do Produto
+        /// </summary>        
         public string Nome { get; set; }
+        /// <summary>
+        /// Quantiade do Produto
+        /// </summary>
         public int Quantidade { get; set; }
+        /// <summary>
+        /// Valor do Produto
+        /// </summary>
         public decimal Valor { get; set; }
+        /// <summary>
+        /// Descrição do Produto
+        /// </summary>
         public string Descricao { get; set; }
     }
 }
